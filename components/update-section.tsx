2fb98bf43e4108bde40d25ab9a6d3c8bf95f9463
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-09 16:12:56 +0200

    Added Update Section

---
+++ components/update-section.tsx
@@ -0,0 +1,30 @@
+import { ClockIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Badge } from "./ui/badge";
+
+interface UpdateProps {
+  title: string;
+  date: Date;
+  description: string;
+}
+export default function UpdateSection(props: UpdateProps) {
+  return (
+    <div>
+      <Separator className="mb-2" />
+      <div className="flex items-center gap-2 text-sm text-muted-foreground">
+        <Badge
+          variant="outline"
+          className="rounded-full px-3 py-1 text-sm font-medium"
+        >
+          {props.title}
+        </Badge>
+        <ClockIcon className="h-4 w-4" />
+        {props.date.toLocaleString("en-US", {
+          dateStyle: "long",
+          timeStyle: "long",
+        })}
+      </div>
+      <p>{props.description}</p>
+    </div>
+  );
+}
