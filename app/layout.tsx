1ddae83c9d98911155bdd96c07e4073a91e66c51
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-10 13:34:37 +0200

    Added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`min-h-screen grid grid-rows-[1fr,auto] ${inter.className}`}
+      >
+        {children}
+        <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+          <p className="text-xs text-muted-foreground">
+            &copy; {new Date().getFullYear()} Peyronnet Group. All rights
+            reserved.
+          </p>
+          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+            <Link
+              href="https://peyronnet.group/privacy"
+              className="text-xs hover:underline underline-offset-4"
+              prefetch={false}
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              href="https://github.com/peyronnet-group/"
+              className="text-xs hover:underline underline-offset-4"
+              prefetch={false}
+            >
+              GitHub
+            </Link>
+            <Link
+              href="https://x.com/PeyronnetGroup"
+              className="text-xs hover:underline underline-offset-4"
+              prefetch={false}
+            >
+              Follow Us
+            </Link>
+          </nav>
+        </footer>
+      </body>
     </html>
   );
 }
