8fa2ca6e0f6df5f3c6a5f27adac0fa5e61d46de0
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-10 19:48:06 +0200

    Added metadata for Incidents page

--- app/incidents/page.tsx
+++ app/incidents/page.tsx
@@ -5,6 +5,13 @@ import { Button } from "@/components/ui/button";
 import { parseAllIncidents } from "@/lib/incidentUtils";
 import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Incidents",
+  description:
+    "View all active and past incidents that impacted Peyronnet Group's systems and services.",
+};
 
 export default async function ViewIncidentsPage() {
   const incidents = await parseAllIncidents();
